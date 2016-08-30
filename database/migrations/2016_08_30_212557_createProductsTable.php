174132a5761a61dbb7c3ba93b53e9517d65af915
acev <aasisvinayak@gmail.com>
2016-08-30 22:41:08 +0100

    product and category migration

---
+++ database/migrations/2016_08_30_212557_createProductsTable.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('product_id',255);
+            $table->string('title',255);
+            $table->string('make',255);
+            $table->string('category_id',255);
+            $table->string('description',255);
+            $table->string('details',255);
+            $table->string('image',255);
+            $table->double('price');
+            $table->smallInteger('status');
+            $table->timestamps();
+//            $table->foreign('category_id')
+//                ->references('category_id')->on('categories')
+//                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('products');
+    }
+}
---
+++ database/migrations/2016_08_30_213500_createCategoriesTable.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('category_id',255);
+            $table->string('title',255);
+            $table->string('parent_id',255);
+            $table->smallInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('categories');
+    }
+}
