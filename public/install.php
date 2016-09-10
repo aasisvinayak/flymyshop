4a304f5660a96d70c87b20539cd608fd94966e96
acev <aasisvinayak@gmail.com>
2016-09-10 01:44:46 +0100

    Fix Typo

--- public/install.php
+++ public/install.php
@@ -1,10 +1,18 @@
 <?php
 
+
+
 $envSample = '.env.example';
 $env = '.env';
 
 if (!copy($envSample, $env)) {
     echo "Error occurred. Please check that the directory has write permissions ";
 }
+
+
+class Shop{
+    
+}
+
 
 ?>
