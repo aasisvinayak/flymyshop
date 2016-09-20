abe6861ea9914294b3c9c0621b172e7f9a7f98e4
acev <aasisvinayak@gmail.com>
2016-09-20 14:15:32 +0100

    Fix bug

--- core/app/User.php
+++ core/app/User.php
@@ -9,6 +9,8 @@ class User extends Authenticatable
 {
     use Billable;
 
+    //TODO: use User::has for user_type
+
     /**
      * The attributes that are mass assignable.
      *
@@ -27,21 +29,35 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function addresses()
     {
         return $this->hasMany('App\Http\Models\Address');
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function payment_cards()
     {
         return $this->hasMany('App\Http\Models\PaymentCard');
     }
 
+    /**
+     * Returns all invoices associated with the user
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function invoices()
     {
         return $this->hasMany('App\Http\Models\Invoice');
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
     public function profile()
     {
         return $this->hasOne('App\Http\Models\UserDetail');
